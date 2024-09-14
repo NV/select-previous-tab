549fbad3d94961e4614529b3ef3bcb96be6e171c
Nikita Vasilyev <me@elv1s.ru>
2024-09-14 16:02:39 -0700

    Fix: unloaded tabs were incorrectly selected after.

--- chrome/background.js
+++ chrome/background.js
@@ -12,7 +12,13 @@ chrome.action.onClicked.addListener(async function() {
 	if (!tabs) {
 		tabs = await chrome.tabs.query({});
 		tabs.sort(function descending(a, b) {
-			return b.lastAccessed - a.lastAccessed;
+			// lastAccessed is undefined for unloaded tabs in Chrome.
+			let delta = (b.lastAccessed || 0) - (a.lastAccessed || 0);
+			if (delta) {
+				return delta;
+			}
+			// Fallback to selecting the last opened tab.
+			return b.id - a.id;
 		});
 	}
 	index++;
